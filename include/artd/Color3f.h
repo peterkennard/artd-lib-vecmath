2ae1d1f22d8bbc639af1e14748dae3cde93cdb3f
peterk <peterk@livingwork.com>
2023-10-22 18:15:52 -0400

    added Color4f to vecmath, dead code removal

---
+++ include/artd/Color3f.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "artd/Vec3f.h"
+
+ARTD_BEGIN
+
+#define INL ARTD_ALWAYS_INLINE
+
+class ARTD_API_LIB_VECMATH Color3f
+    : public glm::vec3
+{
+    typedef glm::vec3 super;
+public:
+    static INL float byteToFloat1(int c) {
+        return(((float)(c & 0x0FF))*(1.0f/255.0f));
+    }
+
+    INL Color3f() {
+    }
+
+    INL Color3f(int r, int g, int b)
+        : super(byteToFloat1(r),byteToFloat1(g),byteToFloat1(b))
+    {}
+
+    INL Color3f(float r, float g, float b)
+         : super(r,g,b)
+    {}
+
+    INL Color3f(const glm::vec3 &src)
+        : super(src)
+    {}
+
+    INL Color3f &set(int r, int g, int b) {
+        this->r = byteToFloat1(r);
+        this->g = byteToFloat1(g);
+        this->b = byteToFloat1(b);
+        return(*this);
+    }
+
+    INL Color3f &set(float r, float g, float b) {
+        this->r = r;
+        this->g = g;
+        this->b = b;
+        return(*this);
+    }
+    INL Color3f &set(const glm::vec3 &src)  {
+        super::operator=(src);
+        return(*this);
+    }
+};
+
+#undef INL
+
+ARTD_END
+
+
