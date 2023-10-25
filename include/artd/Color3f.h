1d26c12ca011b983b87f8d21f59a8f51dcc31952
peterk <peterk@livingwork.com>
2023-10-25 08:19:43 -0400

    added Color4f

--- include/artd/Color3f.h
+++ include/artd/Color3f.h
@@ -31,11 +31,11 @@ public:
     {}
 
     INL Color3f &set(int r, int g, int b) {
-        this->r = byteToFloat1(r);
-        this->g = byteToFloat1(g);
-        this->b = byteToFloat1(b);
-        return(*this);
-    }
+          this->r = byteToFloat1(r);
+          this->g = byteToFloat1(g);
+          this->b = byteToFloat1(b);
+          return(*this);
+      }
 
     INL Color3f &set(float r, float g, float b) {
         this->r = r;
@@ -48,6 +48,54 @@ public:
         return(*this);
     }
 };
+
+class ARTD_API_LIB_VECMATH Color4f
+    : public glm::vec4
+{
+    typedef glm::vec4 super;
+public:
+
+    INL Color4f() {
+    }
+
+    INL Color4f(const glm::vec4 &src)
+        : super(src)
+    {}
+
+    INL Color4f(int r, int g, int b)
+        : super(Color3f::byteToFloat1(r),
+                Color3f::byteToFloat1(g),
+                Color3f::byteToFloat1(b), 1.f)
+    {}
+
+    INL Color4f(int r, int g, int b, int a)
+        : super(Color3f::byteToFloat1(r),
+                Color3f::byteToFloat1(g),
+                Color3f::byteToFloat1(b),
+                Color3f::byteToFloat1(a))
+    {}
+
+    INL Color4f(float r, float g, float b, float a = 1.f)
+         : super(r,g,b,a)
+    {}
+
+    INL Color4f &set(float r, float g, float b, float a = 1.f) {
+        this->r = r;
+        this->g = g;
+        this->b = b;
+        this->a = a;
+        return(*this);
+    }
+    INL Color4f &set(int r, int g, int b, int a = 0x0FF) {
+        this->r = Color3f::byteToFloat1(r);
+        this->g = Color3f::byteToFloat1(g);
+        this->b = Color3f::byteToFloat1(b);
+        this->a = Color3f::byteToFloat1(a);
+        return(*this);
+    }
+
+};
+
 
 #undef INL
 
